124190241cf7ffc8426122f25e890b0c498000de
Aria Nikzad <a.c.nikzad@gmail.com>
2022-05-08 11:52:11 -0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     key: process.env.VUE_APP_MY_API_KEY,
+//     libraries: 'places' // necessary for places input
+//   }
+// });
+
+axios.defaults.baseURL = 
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app');
+
 
 createApp(App).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GmapMap
+      :center="{lat:10, lng:10}"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px">
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      />
+    </GmapMap>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
+const startMarker = ('http://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-Free-PNG-Image.png');
 
 export default {
-  name: 'HomeView',
+  data: function() {
+    return {
+      center: {lat:38.58166, lng:-121.49445},
+      zoom: 13,
+      markers:[],
+      reviews:[],
+      bathrooms: [],
+      markerOptions: {
+        url: startMarker,
+        size: {width: 50, height: 50, f: 'px', b: 'px',},
+        scaledSize: {width: 30, height: 30, f: 'px', b: 'px',}
+      },
+    };
+  },
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+
+  },
+  created: function() {
+    axios.get(`/api/reviews`).then(response => {
+      this.reviews = response.data;
+      console.log(response);
+    });
+
+    axios.get(`/api/bathrooms`).then(response => {
+      this.bathrooms = response.data;
+      console.log(response);
+      console.log(response["data"][0]);
+      this.markers.push({
+        park: response["data"][0]["park"],
+        position: {lat:parseFloat(response["data"][0]["latitude"]), lng:parseFloat(response["data"][0]["longitude"])}});
+      console.log(this.markers);
+    });
+
+
+    
+  },
+};
 </script>
