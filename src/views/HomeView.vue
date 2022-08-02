a3aeddddfe638161e168c741caddc80abd410c6a
Aria Nikzad <a.c.nikzad@gmail.com>
2022-08-02 10:28:47 -0700

    working on create form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,12 +10,15 @@
       </div>
     </div> -->
     <div class="reviews-new">
-      <form v-on:submit.prevent="makeReview()">
-        <p>Bathroom Location: <input type="text" v-model="name"></p>
+      <form v-on:submit.prevent="createReview()">
+        <select id="bathrooms" name="bathroom" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+          <option value="" disabled selected>Select bathroom</option>
+          <option v-for="bathroom in bathrooms" :value="bathroom.id">{{bathroom.label}}</option>
+          </select>
         <p>Comment: <input type="text" v-model="description"></p>
         <p>Would you return? <input type="text" v-model="price"></p>
         <p>Rating out of 5 stars: <input type="text" v-model="price"></p>
-        <button>Make a new product</button>
+        <button class="btn btn-primary btn-lg" id="submitButton" type="submit" value="Submit" v-on:click="createReview()">Submit Review</button>
       </form>
     </div>
 
@@ -37,17 +40,13 @@ export default {
     return {
       reviews: [],
       users: [],
-      bathrooms:[]
+      bathrooms:[],
+      currentBathroom: [],
   };
 },
 
   created: function() {
-    axios.get(`/api/users`, {
-      params: {},
-      headers: {
-        'Content-Type': 'application/json'
-     }     
-    }).then(response => {
+    axios.get("/api/users").then(response => {
       this.users = response.data;
       console.log(response.data);
     });
@@ -69,15 +68,38 @@ export default {
      }     
     }).then(response => {
       this.bathrooms = response.data;
-      console.log(response.data);
+      console.log(this.bathrooms);
     });
     
   },
    methods: {
       showReviews: function() {
         console.log(location.id)
+      },
+
+      selectBathroom: function(theBathroom) {
+      console.log('selecting the bathroom...', theBathroom.target.value);
+      if (theBathroom.target.value) {
+        const id = parseInt(theBathroom.target.value, 10);
+        axios.get(`/api/bathrooms/${id}`).then(response => {
+          console.log('bathroom details ...', response.data);
+          if (response.data) {
+            this.currentBathroom= response.data;
+            }
+          });
+        }
+      },
+
+      createReview: function(event) {
+        event.preventDefault();
+        console.log("Creating the review...")
+        const {bathroomID} = this;
+
+
       }
 
     }
+
+
 };
 </script>
